33078c093d5a3d9ef8a17c1625d819a3cbdf9473
LongquanLiu <773970134@qq.com>
2018-05-07 09:17:31 +0800

    IO_flush

--- main.c
+++ main.c
@@ -1053,7 +1053,7 @@ static void do_server_recv(int f_in, int f_out, int argc, char *argv[])
 
         write_int(f_out, NDX_DONE);
         send_msg(MSG_STATS, (char*)&stats.total_read, sizeof stats.total_read, 0);
-        io_flush(FULL_FLUSH);
+        /*io_flush(FULL_FLUSH);*/
 
         /* Handle any keep-alive packets from the post-processing work
          * that the generator does. */
